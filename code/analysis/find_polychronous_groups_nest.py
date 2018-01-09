e0b58ab7fb5065936b0f94799dae056a8d7b8f3b
Philipp Weidel <p.weidel@fz-juelich.de>
2018-01-10 06:57:02 +0900

    maybe correct version for find groups with nest

--- code/analysis/find_polychronous_groups_nest.py
+++ code/analysis/find_polychronous_groups_nest.py
@@ -49,13 +49,14 @@ Ni = 200  # number of inhibitory neurons
 M = 100  # number of outgoing connections per neuron
 
 
-def build_simulate(stim_target_gids, stim_times, stim_weights, group, t_fired):
-
+def build_simulate(stim_target_gids, stim_times, stim_weights, stim_delay, group, t_fired):
     import nest
 
     global t_sim, N, Ne, Ni, M, min_delay, max_delay, sim_resolution
     global exc_conns, exc_pre, exc_post, exc_weight, exc_delay
     global inh_conns, inh_pre, inh_post, inh_weight, inh_delay
+
+    #print(zip(stim_target_gids, stim_times))
 
     nest.ResetKernel()
     nest.sr("M_ERROR setverbosity")
@@ -86,6 +87,7 @@ def build_simulate(stim_target_gids, stim_times, stim_weights, group, t_fired):
     sgs = dict(zip(stim_times_unique, nest.Create('spike_generator', len(stim_times_unique))))
     # and connect it to the respective targets using the corresponding weights
     for t_stim in sgs.keys():
+        #print("TSTIM", t_stim)
         nest.SetStatus([sgs[t_stim]], {"spike_times": [t_stim]})
         idxs = np.where(stim_times == t_stim)[0]
         if idxs.size:
@@ -108,6 +110,29 @@ def build_simulate(stim_target_gids, stim_times, stim_weights, group, t_fired):
 
     # connect neurons to spike detector
     nest.Connect(all_neurons, sd, 'all_to_all')
+    N_postspikes = np.zeros(1000).astype('int')
+    I_postspikes = np.zeros([1000, 1000])
+    J_postspikes = np.zeros([1000, 1000])
+    D_postspikes = np.zeros([1000, 1000])
+    C_postspikes = np.zeros([1000, 1000])
+    #print group, stim_delay, t_fired 
+
+    for i, t in enumerate(t_fired):
+        for d in range(21):
+            idxs = np.where(np.logical_and(exc_pre == group[i], exc_delay == d))[0]
+            for j, p in enumerate(exc_post[idxs].astype('int')):
+
+                if (exc_weight[idxs][j] > 9.5 or p > Ne) and d >= stim_delay[i]:
+                    timing = int(t + d) -1 ### WHY +1
+
+                    J_postspikes[timing][N_postspikes[timing]] = group[i]
+                    I_postspikes[timing][N_postspikes[timing]] = p 
+                    D_postspikes[timing][N_postspikes[timing]] = d 
+                    C_postspikes[timing][N_postspikes[timing]] = exc_weight[idxs][j]
+                    N_postspikes[timing] += 1
+
+                    #print("timing", timing, "t", t, "J", J_postspikes[timing][N_postspikes[timing]-1]-1, "D", D_postspikes[timing][N_postspikes[timing]-1]-1, "C", C_postspikes[timing][N_postspikes[timing]-1], "I", I_postspikes[timing][N_postspikes[timing]-1]-1, "N", N_postspikes[timing])
+
 
     # simulate for sim_time in steps of rec_time
     nest.Simulate(t_sim)
@@ -116,52 +141,172 @@ def build_simulate(stim_target_gids, stim_times, stim_weights, group, t_fired):
     t_fired.extend(nest.GetStatus(sd, 'events')[0]['times'])
     group.extend(nest.GetStatus(sd, 'events')[0]['senders'])
 
+    for i, t in enumerate(t_fired):
+        if i < 3:
+            continue
+        #print "SPIKE at ", t, "id", group[i]
+        for d in range(21):
+            if group[i] <= Ne:
+                idxs = np.where(np.logical_and(exc_pre == group[i], exc_delay == d))[0]
+                for j, p in enumerate(exc_post[idxs].astype('int')):
+
+                    if (exc_weight[idxs][j] > 9.5 or p > Ne):
+                        timing = int(t + d) -1 ### WHY +1
+
+                        J_postspikes[timing][N_postspikes[timing]] = group[i]
+                        I_postspikes[timing][N_postspikes[timing]] = p 
+                        D_postspikes[timing][N_postspikes[timing]] = d 
+                        C_postspikes[timing][N_postspikes[timing]] = exc_weight[idxs][j]
+                        N_postspikes[timing] += 1
+
+                        #print("SIM timing", timing, "t", t, "J", J_postspikes[timing][N_postspikes[timing]-1]-1, "D", D_postspikes[timing][N_postspikes[timing]-1]-1, "C", C_postspikes[timing][N_postspikes[timing]-1], "I", I_postspikes[timing][N_postspikes[timing]-1]-1, "N", N_postspikes[timing])
+            else:
+                idxs = np.where(np.logical_and(inh_pre == group[i], inh_delay == d))[0]
+                for j, p in enumerate(inh_post[idxs].astype('int')):
+                    #print "INHIBITORY", idxs, group[i], d, p#, np.where(inh_delay == d)[0], np.where(inh_pre == group[i])[0]
+
+                    timing = int(t + d) -1 ### WHY +1
+
+                    J_postspikes[timing][N_postspikes[timing]] = group[i]
+                    I_postspikes[timing][N_postspikes[timing]] = p 
+                    D_postspikes[timing][N_postspikes[timing]] = d 
+                    C_postspikes[timing][N_postspikes[timing]] = inh_weight[idxs][j]
+                    N_postspikes[timing] += 1
+
+                    #print("SIM timing INHIBITORY!!!", timing, "t", t, "J", J_postspikes[timing][N_postspikes[timing]-1]-1, "D", D_postspikes[timing][N_postspikes[timing]-1]-1, "C", C_postspikes[timing][N_postspikes[timing]-1], "I", I_postspikes[timing][N_postspikes[timing]-1]-1, "N", N_postspikes[timing])
+
+
+
+
+
+
+
+
+    layer = [1] * len(group)
+
     # find the links and determine the layers
     N_fired = len(group)
     L_max = 0
     links = []
-    for i in range(3, N_fired):
-        for j in range(i):
-            if group[j] <= Ne:
-                delays = exc_delay[np.where(np.all([exc_pre == group[j], exc_post == group[i]], axis=0))[0]]
-            else:
-                delays = inh_delay[np.where(np.all([inh_pre == group[j], inh_post == group[i]], axis=0))[0]]
-            for d in delays:
-                layer = 2
-                if links:
-                    idxs = np.where(np.array(links)[:, 1] == group[j])[0]
-                    if idxs.size:
-                        layer = int(np.max(np.array(links)[idxs, 3]) + 1)
-                        if layer > L_max:
-                            L_max = layer
-                links.append([group[j], group[i], d, layer])
+    #print t_fired
+    #print "N Fired", N_fired
 
-    if L_max >= 7:
-        # save group in JSON format
-        json_group = {}
-        json_group["N_fired"] = N_fired
-        json_group["L_max"] = L_max
+    if N_fired > 6:
+        for i, t in enumerate(t_fired):
+            t -= 1
+            if i < 3:
+                continue
+            layer[i] = 0
+            #print "I", i, "T", t
+#           for ( p=t_fired[i]; (p>t_fired[i]-latency) & (p>=0); p-- ) // latency=D=20
+            for p in np.arange(t, t-20, -1).astype('int'):
+                if p < 0:
+                    break
 
-        json_fired = []
-        for i in range(N_fired):
-            json_fire = {}
-            json_fire["neuron_id"] = group[i]
-            json_fire["t_fired"] = t_fired[i]
-            json_fired.append(json_fire)
-        json_group["fired"] = json_fired
+                #print "P", p, " len N postspikes", N_postspikes[p]
+                
+#             for ( j=0; j<N_postspikes[p]; j++ )
+                for j in range(N_postspikes[p]):
+                    #print "J", j
+#               if ( (I_postspikes[p][j]==group[i]) & (J_postspikes[p][j]<Ne) )
+#               {
 
-        json_links = []
-        for j in range(len(links)):
-            json_link = {}
-            json_link["pre"] = links[j][0]
-            json_link["post"] = links[j][1]
-            json_link["delay"] = links[j][2]
-            json_link["layer"] = links[j][3]
-            json_links.append(json_link)
-        json_group["links"] = json_links
+                    if (I_postspikes[p][j]==group[i]) and (J_postspikes[p][j]<Ne):
+#                 for ( k=0; k<i; k++ )
+#                {
+#           	    if ( (group[k]==J_postspikes[p][j]) & (layer[k]+1>layer[i]) ){
+#           	      layer[i]=layer[k]+1;
+#
+#                }
+#                }
+                        for k in range(i):
+                            if group[k] == J_postspikes[p][j] and layer[k]+1 > layer[i]:
+                                layer[i] = layer[k]+1
 
-        print("group found", json_group)
-        return json_group
+                                if layer[i] > L_max:
+                                    L_max = layer[i]
+
+
+                        links.append([J_postspikes[p][j], I_postspikes[p][j], D_postspikes[p][j], layer[i]])
+                        #print "new link", t, group[j], group[i], delay, layer[i]
+
+
+
+#                   {
+#
+#           	  links[N_links][0]=J_postspikes[p][j];
+#           	  links[N_links][1]=I_postspikes[p][j];
+#           	  links[N_links][2]=D_postspikes[p][j];
+#           	  links[N_links++][3]=layer[i];
+#           	  if ( L_max < layer[i] )
+#           	  L_max = layer[i];
+#                 }
+
+            
+
+
+        #for i in range(3, N_fired):
+        #    for j in range(i):
+        #        if group[j] <= Ne:
+        #            delays = exc_delay[np.where(np.all([exc_pre == group[j], exc_post == group[i]], axis=0))[0]]
+        #        else:
+        #            delays = inh_delay[np.where(np.all([inh_pre == group[j], inh_post == groupt[i]], axis=0))[0]]
+        #        for d in delays:
+        #            layer = 2
+        #            if links:
+        #                idxs = np.where(np.array(links)[:, 1] == group[j])[0]
+        #                if idxs.size:
+        #                    layer = int(np.max(np.array(links)[idxs, 3]) + 1)
+        #                    if layer > L_max:
+        #                        L_max = layer
+        #            links.append([group[j], group[i], d, layer])
+
+
+   #     for i in range(3, N_fired):
+   #         for j in range(i):
+   #             if group[j] <= Ne:
+   #                 delays = exc_delay[np.where(np.all([exc_pre == group[j], exc_post == group[i]], axis=0))[0]]
+   #             else:
+   #                 delays = inh_delay[np.where(np.all([inh_pre == group[j], inh_post == group[i]], axis=0))[0]]
+   #             for d in delays:
+   #                 layer = 2
+   #                 if links:
+   #                     idxs = np.where(np.array(links)[:, 1] == group[j])[0]
+   #                     if idxs.size:
+   #                         layer = int(np.max(np.array(links)[idxs, 3]) + 1)
+   #                         if layer > L_max:
+   #                             L_max = layer
+   #                 links.append([group[j], group[i], d, layer])
+
+        if L_max >= 7:
+            # save group in JSON format
+            json_group = {}
+            json_group["N_fired"] = N_fired
+            json_group["L_max"] = L_max
+
+            json_fired = []
+            for i in range(N_fired):
+                json_fire = {}
+                json_fire["neuron_id"] = group[i]
+                json_fire["t_fired"] = t_fired[i]
+                json_fired.append(json_fire)
+            json_group["fired"] = json_fired
+
+            json_links = []
+            for j in range(len(links)):
+                #### TODO pre and post are reversed in order?
+                json_link = {}
+                json_link["pre"] = links[j][0]-1
+                json_link["post"] = links[j][1]-1
+                json_link["delay"] = links[j][2]-1
+                json_link["layer"] = links[j][3]
+                json_links.append(json_link)
+            json_group["links"] = json_links
+
+            #print("group found")
+            #for l in json_group['links']:
+                #print l
+            return json_group
 
     return None
 
@@ -176,15 +321,27 @@ def worker(pivot_neuron):
 
     local_json_data = []
 
-    print(pivot_neuron)
+    #print("pivot neuron", pivot_neuron)
 
     inc_exc_conns = exc_conns[np.where(exc_post == pivot_neuron)[0]]
+    #print("exc conns", inc_exc_conns)
     for stim_triplet in itertools.combinations(inc_exc_conns, 3):
+
+       # if not stim_triplet[0]['pre'] in [5, 32, 63]:
+       #     continue
+       # if not stim_triplet[1]['pre'] in [5, 32, 63]:
+       #     continue
+       # if not stim_triplet[2]['pre'] in [5, 32, 63]:
+       #     continue
+
+        #print(" STIM TRIP", stim_triplet)
         stim_target_gids = []
         stim_times = []
         stim_weights = []
+
         group = []
         t_fired = []
+        group_delays = []
         max_delay_triplet = np.max(np.array([c['delay'] for c in stim_triplet]))
         for st in sorted(stim_triplet, key=lambda x: x['delay'], reverse=True):
             # determine the initially activated connections
@@ -201,10 +358,16 @@ def worker(pivot_neuron):
             stim_weights.extend(stim_weight)
             # store triplet as first three neurons that fire in this group
             group.append(int(st['pre']))
-            t_fired.append(stim_offset)
+            #print ("extend t stim", stim_offset+1-1)
+            t_fired.append(stim_offset+1)
+            group_delays.append(st['delay'])
+            order = np.argsort(group)
+            sorted_group = np.array(group)[order].tolist()
+            sorted_t_fired = np.array(t_fired)[order].tolist()
+            sorted_stim_delay = np.array(group_delays)[order].tolist()
 
         json_group = build_simulate(np.array(stim_target_gids), np.array(
-            stim_times), np.array(stim_weights), group, t_fired)
+            stim_times), np.array(stim_weights), np.array(sorted_stim_delay), sorted_group, sorted_t_fired)
         if not json_group == None:
             local_json_data.append(json_group)
 
@@ -215,9 +378,16 @@ json_data = []
 
 pool = Pool(processes=max_num_processes)
 
-for found_groups in pool.imap_unordered(worker, range(1, Ne+1)):
+for found_groups in pool.imap_unordered(worker, range(1, Ne)):
     json_data += found_groups
 
 with open(out_fn, "w+") as f:
     json.dump(json_data, f)
+
+
+stwd_data = []
+for c in final_stdw:
+    stwd_data.append([c['pre'], c['post'], c['weight'], c['delay']]) 
+
+np.savetxt("stwd_data.dat", stwd_data) 
 
