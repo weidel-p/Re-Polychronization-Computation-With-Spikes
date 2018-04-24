52a0117ef9494a0fa519c7fb64472f1132c43c25
Philipp Weidel <p.weidel@fz-juelich.de>
2018-04-24 15:15:39 +0200

    include resolution to stdp window

--- code/analysis/plot_stdp_window.py
+++ code/analysis/plot_stdp_window.py
@@ -16,14 +16,14 @@ plt.figure(figsize=[16,10])
 for i in args.input:
     with open(i, 'r+') as f:
         data = json.load(f)
-        if 'quali' in data['label'] or 'bitwise' in data['label'] or 'initial' in data['label']:
+        if 'quali' in data['label'] or 'bitwise' in data['label'] or 'initial' in data['label'] or 'reso' in data['label']:
             plt.plot(data['dt'], data['dw'], label=data['label'])
 
 plt.axhline(0, color='k', linestyle='--')
 plt.axvline(0, color='k', linestyle='--')
 plt.legend()
 plt.xlabel("dt")
-plt.ylabel("dw")
+plt.ylabel("dw/w")
 plt.savefig(args.output)
 
 
--- code/analysis/stdp_window.py
+++ code/analysis/stdp_window.py
@@ -44,11 +44,11 @@ def init(cfg):
     if cfg["network-params"]["plasticity"]["synapse-model"] == 'stdp_izh_synapse':
         nest.CopyModel(cfg["network-params"]["plasticity"]["synapse-model"], "EX", {
             'weight': cfg["network-params"]["plasticity"]['W_init'],
-            'Wmax': cfg["network-params"]["plasticity"]['Wmax'],
-            'LTP': cfg["network-params"]["plasticity"]['LTP'],
-            'LTD': cfg["network-params"]["plasticity"]['LTD'],
+            'Wmax': cfg["network-params"]["plasticity"]['Wmax'] / 100., # we devide by 100 to see less impact of the presynaptic spike on the postsynapic neuron
+            'LTP': cfg["network-params"]["plasticity"]['LTP'] / 100.,   # this is possible because in the end, we measure the relative weight change
+            'LTD': cfg["network-params"]["plasticity"]['LTD'] / 100.,
             "tau_syn_update_interval": cfg["network-params"]["plasticity"]["tau_syn_update_interval"],
-            "constant_additive_value": cfg["network-params"]["plasticity"]["constant_additive_value"],
+            "constant_additive_value": cfg["network-params"]["plasticity"]["constant_additive_value"] / 100.,
             "reset_weight_change_after_update": cfg["network-params"]["plasticity"]["reset_weight_change_after_update"]
     
         })
@@ -88,23 +88,31 @@ def stim(dt, cfg):
 
     pre = nest.Create("izhikevich")
     post = nest.Create("izhikevich")
+
+    Wmax = nest.GetDefaults("EX")['Wmax']
+    init_weight = Wmax / 2.
     
-    nest.Connect(pre, post, 'all_to_all', {'model': 'EX', 'weight': 1., 'delay': 1.})
+    nest.Connect(pre, post, 'all_to_all', {'model': 'EX', 'weight': init_weight, 'delay': 1.})
     
     sg_pre = nest.Create("spike_generator", 1, {"spike_times": [500., 900.]})
     sg_post = nest.Create("spike_generator", 1, {"spike_times": [500. + dt]})
 
     sd = nest.Create("spike_detector", 1, {"to_file": True})
  
-    nest.Connect(sg_pre, pre, 'all_to_all', {'model': 'static_synapse', 'weight': 20.})
-    nest.Connect(sg_post, post, 'all_to_all', {'model': 'static_synapse', 'weight': 20.})
+    nest.Connect(sg_pre, pre, 'all_to_all', {'model': 'static_synapse', 'weight': 100*2*Wmax})
+    nest.Connect(sg_post, post, 'all_to_all', {'model': 'static_synapse', 'weight': 100*2*Wmax})
 
     nest.Connect(pre, sd)
     nest.Connect(post, sd)
 
     nest.Simulate(1010)
 
-    return(nest.GetStatus(nest.GetConnections(pre, post), 'weight')[0] - 1)
+    new_weight = nest.GetStatus(nest.GetConnections(pre, post), 'weight')[0] 
+
+    print(dt, Wmax, init_weight, new_weight)
+
+    return (new_weight - init_weight) / init_weight
+
     
 
 def simulate(cfg):
@@ -123,7 +131,7 @@ def simulate(cfg):
     
     ws = []
 
-    for t in range(100):
+    for t in range(1):
         nest.Simulate(1010)
 
         ws.append(nest.GetStatus(nest.GetConnections(pre, post), 'weight')[0])
