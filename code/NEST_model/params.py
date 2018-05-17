7fe4b4c56dc37f9622a9e4868af38a9f3a333880
rpauli <r.pauli@fz-juelich.de>
2018-05-17 12:44:38 +0200

    fixed an integer error

--- code/NEST_model/params.py
+++ code/NEST_model/params.py
@@ -4,8 +4,8 @@ N = 1000
 N_inh = 200
 N_ex = 800
 N_syn = 100
-tau_plus = 20
-tau_minus = 20
+tau_plus = 20.
+tau_minus = 20.
 Aplus = 0.1
 Aminus = 0.12
 multapses = False
@@ -17,7 +17,8 @@ exc_neuron_model = {'consistent_integration': False,
                     'b': 0.2,
                     'c': -65.0,
                     'a': 0.02,
-                    'd': 8.0}
+                    'd': 8.0,
+                    'tau_minus': tau_minus}
 inh_neuron_model = {'consistent_integration': False,
                     'V_th': 30.,
                     'U_m': -0.2 * 65.0,
