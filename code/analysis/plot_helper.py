527906fbc3d16e3224ad842bc51f5b98f07f098e
rpauli <r.pauli@fz-juelich.de>
2018-05-22 10:33:54 +0200

    added latexify

--- code/analysis/plot_helper.py
+++ code/analysis/plot_helper.py
@@ -9,6 +9,59 @@ import json
 import ijson
 import os
 
+
+def latexify(fig_width=None, fig_height=None, columns=1):
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    columns : {1, 2}
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    assert (columns in [1, 2])
+
+    if fig_width is None:
+        fig_width = 3.39 if columns == 1 else 6.9  # width in inches
+
+    if fig_height is None:
+        golden_mean = (np.sqrt(5) - 1.0) / 2.0  # Aesthetic ratio
+        fig_height = fig_width * golden_mean  # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + fig_height +
+              "so will reduce to" + MAX_HEIGHT_INCHES + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    params = {'backend': 'ps',
+              'text.usetex': True,
+
+
+
+              'text.latex.preamble': [r'\usepackage{gensymb}',r'\let\savedegree\degree',r'\let\degree\relax',r'\usepackage{mathabx}','\let\degree\savedegree'],
+              'axes.labelsize': 10,  # fontsize for x and y labels (was 10)
+              'axes.titlesize': 10,
+              'font.size': 10,  # was 10
+              'legend.fontsize': 10,  # was 10
+              'xtick.labelsize': 10,
+              'ytick.labelsize': 10,
+              'figure.figsize': [fig_width, fig_height],
+              'font.family': 'sans-serif',
+              'savefig.dpi': 300,
+              'lines.linewidth': 2
+              }
+
+    matplotlib.rcParams.update(params)
+
+
 def set_box_color(bp, color):
     plt.setp(bp['boxes'], color=color)
     plt.setp(bp['whiskers'], color=color)
@@ -220,7 +273,10 @@ def return_NTL(groupfile):
     L_list = []
     T_list = []
     i = 0
+
     for i, g in enumerate(groups):
+        if g=={'Failed':1}:
+            return False
         times, senders = hf.get_t_s(g)
 
         N_list.append(int(g["N_fired"]))
